bbc560b76f2fa0bf829695cd6803d4a52c1f2d58
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-12-13 19:51:45 +0900

    Create 5_전처리기_선행처리문.cpp

---
+++ 5_전처리기_선행처리문.cpp
@@ -0,0 +1,20 @@
+//
+//  5_전처리기_선행처리문.cpp
+//  CProject
+//
+//  Created by 오나현 on 12/13/24.
+//
+
+#include <stdio.h>
+
+/// 전처리 구문 (# 친구들)
+/// 가장 먼저 처리되는 구문을 의미한다.
+/// #include, #define, #ifdef DEBUG, #endif (DEBUG), #else, #pragma
+
+// #include <stdio.h>
+
+// const float PI = 3.14f;
+// #define PI 3.14
+// #define VAR 10
+// #define ONE 1
+// #define TWO 2	// ;은 찍지 않는다.
