27e3b9fa74251f7371d46a78a68c37cabe72edc9
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-12-16 19:26:33 +0900

    Update 6_메크로_인라인함수.cpp

--- 6_메크로_인라인함수.cpp
+++ 6_메크로_인라인함수.cpp
@@ -62,10 +62,43 @@ int main()
 
 	return 0;
 }
-*/
 
 // --------------------------------------------------
 
 /// 시간 날짜 관련 define
+/// 문제점을 빠르게 찾을 수 있다.
 
+//__DATE__		// 날짜를 mm.dd.yyyy
+//__TIME__		// 시간을 hh:mm:ss
+//__FILE__		// 현재 소스 파일의 경로와 이름을 나타내는 문자열
+//__LINE__		// 처리 중인 라인 번호를 나타내는 문자열
+//__STDC_HOSTED__ // 호스트 환경이면 1, 아니면 0
 
+int main()
+{
+	printf("날짜 : %s\n", __DATE__);
+	printf("시간 : %s\n", __TIME__);
+	printf("소스 파일에서 처리 중인 라인 번호 : %d\n", __LINE__);
+	printf("호스트인가? : %d\n", __STDC_HOSTED__);
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// 인라인 함수
+
+__inline int square(int a)
+{
+	return a * a;
+}
+
+int main()
+{
+	printf("square(3 + 1) : %d\n", square(3 + 1));
+	// 인라인 처리를 하게 되면 함수로 이동했다가 오는 것이 아닌 그냥 그 값을 바로 출력할 수 있게 도와준다.
+	// 의미 : printf("square(3 + 1) : %d\n", square(3 + 1){ return a * a; }); // 속도가 더욱 빨라진다.
+
+	return 0;
+}
+*/
