00913188a72befebe67535e021de08801b377695
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-12-17 19:58:15 +0900

    Create 0_시작.cpp

---
+++ CppProject/0_시작.cpp
@@ -0,0 +1,39 @@
+//
+//  0_시작.cpp
+//  CppProject
+//
+//  Created by 오나현 on 12/17/24.
+//
+
+/*
+/// 기본 Cpp 구조
+
+// #include <iostream.h>   // 1988년 이전
+#include <iostream>        // 최신 (h가 없음)
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
+
+// --------------------------------------------------
+
+/// 입력 & 출력
+
+#include <iostream>
+
+int main()
+{
+    char name[10];
+    int age;
+
+    std::cin >> name >> age; // 입력받기
+
+    std::cout << "나의 이름은 " << name << "이고, 나이는 " << age << "살 입니다." << std::endl; // 출력하기
+
+    return 0;
+}
+*/
