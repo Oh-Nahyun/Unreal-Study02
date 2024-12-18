03fb9c9cc43026fbc5051e72c078bd2c07cc0657
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-12-18 19:12:52 +0900

    Update 5_클래스.cpp

--- CppProject/5_클래스.cpp
+++ CppProject/5_클래스.cpp
@@ -5,12 +5,13 @@
 //  Created by 오나현 on 12/18/24.
 //
 
+/*
 /// 클래스 (Class)
 
 /// C 언어에서는 구조체, 포인터
 /// C++ 언어에서는 클래스
 
-/// 객체 지향 프로그래밍 (OOP; Object Oriented Programing)
+/// 객체 지향 프로그래밍 (OOP:Object Oriented Programing)
 /// : 실세계의 사물을 추상화(Abstraction)하고,
 ///   캡슐화(Encapsulation) 하며,
 ///   계층 구조는 상속(Inheritance)을 시키며,
@@ -26,4 +27,67 @@
 // --------------------------------------------------
 
 /// 클래스의 원형
+//class 클래스명
+//{
+//	// 멤버 변수(상태:State), 멤버 함수(행동:Behavior) 등
+//};
 
+#include <iostream>
+
+using namespace std;
+
+class Student // 클래스는 주로 헤더파일로 들어간다.
+{
+private:
+    const char* name;
+    int id;
+    int age;
+    const char* phoneNumber;
+
+public:
+    void SetInfo(const char* _name, int _id, int _age, const char* _phoneNumber);
+    void GetShow();
+    void SetId(int _id);
+    void SetAge(int _age);
+};
+
+int main()
+{
+    Student s;
+
+    s.SetInfo("홍길동", 123456789, 20, "010-1234-5678");
+    s.GetShow();
+
+    s.SetId(987654321);
+    s.GetShow();
+
+    return 0;
+}
+
+// 아래 함수들은 주로 Cpp 파일로 들어간다.
+void Student::SetInfo(const char* _name, int _id, int _age, const char* _phoneNumber)
+{
+    name = _name;
+    id = _id;
+    age = _age;
+    phoneNumber = _phoneNumber;
+}
+
+void Student::GetShow()
+{
+    cout << "이름 : " << name;
+    cout << ", 학번 : " << id;
+    cout << ", 나이 : " << age;
+    cout << ", 전화번호 : " << phoneNumber << endl;
+}
+
+void Student::SetId(int _id)
+{
+    id = _id;
+}
+
+void Student::SetAge(int _age)
+{
+    age = _age;
+}
+*/
