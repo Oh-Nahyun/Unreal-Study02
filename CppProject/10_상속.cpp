67025b958abc995695b1211f7e91a077a9c3ac3f
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-12-20 19:18:15 +0900

    Update 10_상속.cpp

--- CppProject/10_상속.cpp
+++ CppProject/10_상속.cpp
@@ -88,9 +88,131 @@ int main()
 // --------------------------------------------------
 
 /// 클래스의 다양한 명칭
-// 1. 상위 클래스(예:Human) : 하위 클래스 (예:Student)
-// 2. 부모 (Parent) 클래스  : 자식 (Child) 클래스
-// 3. 슈퍼 (Super) 클래스   : 서브 (Sub) 클래스
-// 4. 기초 (Basic) 클래스   : 유도 (Derived) 클래스
-*/
+// 1. 상위 클래스(예:Human) :  하위 클래스 (예:Student)
+// 2. 부모 (Parent) 클래스  :  자식 (Child) 클래스
+// 3. 슈퍼 (Super) 클래스   :  서브 (Sub) 클래스
+// 4. 기본 (Base) 클래스    :  유도 (Derived) 클래스
 
+// --------------------------------------------------
+
+/// 멤버 초기화 (이니셜라이즈 : Member Initializer)
+
+#include <iostream>
+
+using namespace std;
+
+class memInit
+{
+private:
+	int num1;
+	int num2;
+public:
+	memInit(int num1, int num2) : num1(num1), num2(num2) {} // 초기화 (상속이 아니니 주의할 것!)
+
+	void ShowInfo() { cout << num1 << "," << num2 << endl; }
+};
+
+int main()
+{
+	memInit mi(50, 70);
+
+	mi.ShowInfo();
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// private, protected, public 상속
+
+/// [1]
+
+#include <iostream>
+
+using namespace std;
+
+class Parent
+{
+private:
+	int num1;
+public:
+	int num2;
+protected:
+	int num3;
+};
+
+class Child : private Parent {};	// 상속 접근제한자가 private인 경우
+
+int main()
+{
+	Child ch;
+
+	cout << ch.num1 << endl;	// Error
+	cout << ch.num2 << endl;	// Error
+	cout << ch.num3 << endl;	// Error
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// [2]
+
+#include <iostream>
+
+using namespace std;
+
+class Parent
+{
+private:
+	int num1;
+public:
+	int num2;
+protected:
+	int num3;
+};
+
+class Child : protected Parent {};	// 상속 접근제한자가 protected인 경우
+
+int main()
+{
+	Child ch;
+
+	cout << ch.num1 << endl;	// Error
+	cout << ch.num2 << endl;	// Error
+	cout << ch.num3 << endl;	// Error
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// [3]
+
+#include <iostream>
+
+using namespace std;
+
+class Parent
+{
+private:
+	int num1;
+public:
+	int num2;
+protected:
+	int num3;
+};
+
+class Child : public Parent {};	// 상속 접근제한자가 public인 경우
+
+int main()
+{
+	Child ch;
+
+	cout << ch.num1 << endl;	// Error
+	cout << ch.num2 << endl;	// Ok
+	cout << ch.num3 << endl;	// Error
+
+	return 0;
+}
+*/
