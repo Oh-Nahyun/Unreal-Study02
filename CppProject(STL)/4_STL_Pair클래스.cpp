0a424ac46add60700756ead5e31c185f988b7910
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-02 19:43:19 +0900

    Create 4_STL_Pair클래스.cpp

---
+++ CppProject(STL)/4_STL_Pair클래스.cpp
@@ -0,0 +1,24 @@
+//
+//  4_STL_Pair클래스.cpp
+//  CppProject
+//
+//  Created by 오나현 on 01/02/25.
+//
+
+/// pair 클래스
+/// 두 객체를 하나의 객체로 취급할 수 있도록 묶어주는 클래스
+/// STL에서 데이터 쌍을 표현할 때 사용
+/// #include <utility>에 존재
+
+/// pair 클래스의 구조
+//template <class T1, class T2> struct pair;
+//template <typename T1, typename T2> struct pair;
+//
+//T1 : first
+//T2 : second
+//로 구분한다.
+
+/// 멤버 함수 및 간단한 사용법
+
+
+// --------------------------------------------------
