54f4240365bd5b6e8b7bef13b0cfd981eb4e1eee
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-02 19:50:05 +0900

    Update 4_STL_Pair클래스.cpp

--- CppProject(STL)/4_STL_Pair클래스.cpp
+++ CppProject(STL)/4_STL_Pair클래스.cpp
@@ -5,20 +5,45 @@
 //  Created by 오나현 on 01/02/25.
 //
 
+/*
 /// pair 클래스
 /// 두 객체를 하나의 객체로 취급할 수 있도록 묶어주는 클래스
 /// STL에서 데이터 쌍을 표현할 때 사용
 /// #include <utility>에 존재
 
 /// pair 클래스의 구조
-//template <class T1, class T2> struct pair;
-//template <typename T1, typename T2> struct pair;
-//
-//T1 : first
-//T2 : second
-//로 구분한다.
-
-/// 멤버 함수 및 간단한 사용법
-
+// template <class T1, class T2> struct pair;
+// template <typename T1, typename T2> struct pair;
+// T1 : first
+// T2 : second 로 구분한다.
 
 // --------------------------------------------------
+
+/// 멤버 함수 및 간단한 사용법
+// pair<[type1], [type2]> p;
+// 사용할 데이터 타입 1, 2를 넣고 그 타입의 pair 클래스인 p를 만든다.
+
+// p.first  : p의 첫번째 인자를 반환해준다.
+// p.second : p의 두번째 인자를 반환해준다.
+
+// make_pair(변수1, 변수2) : 변수1과 변수2가 들어간 pair를 만들어준다.
+
+// operator로 (==, !=, <, >, <=, >=)가 정의되어 있어 사용 가능하다.
+*/
+
+// --------------------------------------------------
+
+/// 사용 예제 (대소 비교) (int, string인 경우)
+
+#include <iostream>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
