7e492f367c802c962030dc20855eaa83a3257363
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-03 19:51:06 +0900

    Create 6_STL_멀티셋.cpp

---
+++ CppProject(STL)/6_STL_멀티셋.cpp
@@ -0,0 +1,27 @@
+//
+//  6_STL_멀티셋.cpp
+//  CppProject
+//
+//  Created by 오나현 on 01/03/25.
+//
+
+/// Multiset Container
+/// set과 구별되는 multiset의 가장 큰 특징은 key 값이 중복된다는 점이다.
+
+/// 1. 소 HP 포션
+/// 2. 중 HP 포션
+/// 3. 대 HP 포션
+
+// --------------------------------------------------
+
+/// 
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
