8569d6dd208b369ae53abf07fd5169f982779053
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-13 18:44:31 +0900

    Update 8_STL_멀티맵.cpp

--- CppProject(STL)/8_STL_멀티맵.cpp
+++ CppProject(STL)/8_STL_멀티맵.cpp
@@ -47,7 +47,7 @@ int main()
     multimap<int, int> mm;
     pair<multimap<int, int>::iterator, multimap<int, int>::iterator> pair_iter = mm.equal_range(6);
 
-    return 0
+    return 0;
 }
 
 // --------------------------------------------------
@@ -83,6 +83,132 @@ int main()
     }
     cout << endl << endl;
 
-    return 0
+    // --------------------------------------------------
+
+    // 중복 인자 2개 추가
+    mm.insert(pair<int, int>(40, 44));
+    mm.insert(pair<int, int>(40, 3));
+
+    // 접근 방법 (확인)
+    for (iter = mm.begin(); iter != mm.end(); iter++)
+    {
+        cout << "[ " << iter->first << ", " << iter->second << " ]";
+    }
+    cout << endl << endl;
+
+    // --------------------------------------------------
+
+    // 정보 출력
+    cout << "size() : " << mm.size() << endl;
+    cout << "count(40) : " << mm.count(40) << endl << endl;
+
+    return 0;
+}
+
+// --------------------------------------------------
+
+/// 예제
+/// upper_bound, lower_bound
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    multimap<int, string> mm;
+
+    mm.insert(pair<int, string>(60, "C"));
+    mm.insert(pair<int, string>(30, "C++"));
+    mm.insert(pair<int, string>(40, "Linux"));
+    mm.insert(pair<int, string>(90, "JAVA"));
+    mm.insert(pair<int, string>(40, "Ubunto"));
+    mm.insert(pair<int, string>(20, "Unix"));
+    mm.insert(pair<int, string>(40, "CentOS"));
+
+    multimap<int, string>::iterator iter;
+
+    // 접근 방법 (확인)
+    for (iter = mm.begin(); iter != mm.end(); iter++)
+    {
+        cout << "[ " << iter->first << ", " << iter->second << " ]";
+    }
+    cout << endl << endl;
+
+    // --------------------------------------------------
+
+    // 중복 키 값 출력 (40)
+    for (iter = mm.lower_bound(40); iter != mm.upper_bound(40); iter++)
+    {
+        cout << "[ " << iter->first << ", " << iter->second << " ]";
+    }
+    cout << endl << endl;
+
+    // 인자 >= lower_bound
+    // 인자 < upper_bound
+    // [ lower_bound, upper_bound ) 개폐구간
+
+    multimap<int, string>::iterator lower_iter = mm.lower_bound(40);
+    multimap<int, string>::iterator upper_iter = mm.upper_bound(40);
+
+    cout << "lower_iter : " << lower_iter->first << ", " << lower_iter->second << endl;
+    cout << "upper_iter : " << upper_iter->first << ", " << upper_iter->second << endl;
+
+    return 0;
 }
 */
+
+// --------------------------------------------------
+
+/// 예제
+/// equal_range (pair)
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    multimap<int, string> mm;
+
+    mm.insert(pair<int, string>(60, "C"));
+    mm.insert(pair<int, string>(30, "C++"));
+    mm.insert(pair<int, string>(40, "Linux"));
+    mm.insert(pair<int, string>(90, "JAVA"));
+    mm.insert(pair<int, string>(40, "Ubunto"));
+    mm.insert(pair<int, string>(20, "Unix"));
+    mm.insert(pair<int, string>(40, "CentOS"));
+
+    multimap<int, string>::iterator iter;
+
+    // 접근 방법 (확인)
+    for (iter = mm.begin(); iter != mm.end(); iter++)
+    {
+        cout << "[ " << iter->first << ", " << iter->second << " ]";
+    }
+    cout << endl << endl;
+
+    // --------------------------------------------------
+
+    // 중복 키 값 출력 (40)
+    for (iter = mm.equal_range(40).first; iter != mm.equal_range(40).second; iter++)
+    {
+        cout << "[ " << iter->first << ", " << iter->second << " ]";
+    }
+    cout << endl << endl;
+
+    // 인자 >= equal_range.first
+    // 인자 < equal_range.second
+    // [ first, second ) 개폐구간
+
+    pair<multimap<int, string>::iterator, multimap<int, string>::iterator> pair_iter = mm.equal_range(40);
+
+    cout << "pair_iter.first : " << (pair_iter.first)->first << ", " << (pair_iter.first)->second << endl;
+    cout << "pair_iter.second : " << (pair_iter.second)->first << ", " << (pair_iter.second)->second << endl;
+
+    return 0;
+}
