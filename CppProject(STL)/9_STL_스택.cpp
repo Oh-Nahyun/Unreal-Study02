9efa0c845b941322ae54320821d2a95110a884a9
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-13 19:01:01 +0900

    Create 9_STL_스택.cpp

---
+++ CppProject(STL)/9_STL_스택.cpp
@@ -0,0 +1,40 @@
+//
+//  9_STL_스택.cpp
+//  CppProject
+//
+//  Created by 오나현 on 01/13/25.
+//
+
+/*
+/// Stack Container
+/// LIFO 방식 (Last In First Out)
+
+/// adapter 방식 : vector, deque, list container 에 붙여서 (기반으로) 사용이 가능하다.
+
+/// 내부적으로 vector, deque, list 구조로 구현이 되어있으나, 
+/// stack 이라는 포장지로 잘 감싸서 stack 과 같이 작동하도록 멤버 함수 등을 지원해주는 것
+
+/// default 는 deque container 기반으로 작동한다.
+
+// --------------------------------------------------
+
+/// Stack 사용법
+
+#include <iostream>
+#include <string>
+#include <stack>		// <stack> 헤더파일
+
+using namespace std;
+
+int main()
+{
+	// 생성 방법
+	stack<int> st1;
+	stack<string> st2;
+
+	// 내부 컨테이너 구조를 바꾸는 형식
+
+
+	return 0;
+}
+*/
