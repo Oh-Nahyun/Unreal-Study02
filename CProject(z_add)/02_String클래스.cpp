43bd03ff7419a00d9f761d9b0ec82a60f28f9157
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-01-15 19:10:24 +0900

    Update 02_String클래스.cpp

--- CProject(z_add)/02_String클래스.cpp
+++ CProject(z_add)/02_String클래스.cpp
@@ -23,6 +23,7 @@ using namespace std;
 int main()
 {
 	int n;
+	char c;
 
 	// 생성
 	string str("이름이름");
@@ -82,10 +83,234 @@ int main()
 	// 대입
 	str1 = str2;
 
+	// --------------------------------------------------
+
+	// 기본
+	str.push_back(c);
+	str.pop_back();
+	str.begin();
+	str.end();
+
 	return 0;
 }
-*/
 
 // --------------------------------------------------
 
-/// 
+/// String 기본 사용법
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "String";
+	string str2("abcdefghijkl");
+	string str3(str1);
+
+	// 확인 출력
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// push_back, pop_back
+	str1.push_back('1');
+	cout << str1 << endl;
+	str1.push_back('2');
+	cout << str1 << endl;
+	cout << endl;
+
+	str1.pop_back();
+	cout << str1 << endl;
+	str1.pop_back();
+	cout << str1 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// operator+ string 뎃셈
+	cout << str1 + str2 << endl;
+	cout << endl;
+
+	str1 += str2;
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// 사이즈 체크
+	cout << str1.size() << endl;
+	cout << str1.length() << endl;
+	cout << str1.capacity() << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// 사이즈 변경
+	str2.resize(4);
+	cout << str2 << endl;
+	cout << str2.size() << endl;
+	cout << str2.length() << endl;
+	cout << str2.capacity() << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// 클리어
+	str2.clear();
+	cout << str2 << endl;
+	cout << str2.size() << endl;
+	cout << str2.length() << endl;
+	cout << str2.capacity() << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// String 추가 사용법 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "String";
+	string str2("abcdefghijkl");
+	string str3(str1);
+
+	// 확인 출력
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// substr
+	cout << str1.substr(5) << endl;
+	cout << str1.substr(3, 2) << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// replace
+	cout << str1.replace(3, 2, str2) << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// swap
+	str3 = "C++ example";
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl << endl;
+
+	swap(str2, str3);
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << str3 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// 검색 (find, operator[], at(), front, back)
+
+	// find
+	str1 = "string";
+	str2 = "abcdefghijkl";
+
+	cout << str1.find("st") << endl;
+	cout << str2.find("cdef") << endl;
+	cout << str2.find("cdef", 5) << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// operator[]
+	cout << str1[0] << endl;
+	cout << str1[3] << endl;
+	cout << str1[str1.size() - 1] << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// at()
+	cout << str1.at(0) << endl;
+	cout << str1.at(3) << endl;
+	cout << str1.at(str1.size() - 1) << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// front, back
+	cout << str1[0] << endl;
+	cout << str1.at(0) << endl;
+	cout << str1.front() << endl << endl;
+
+	cout << str1[str1.size() - 1] << endl;
+	cout << str1.at(str1.size() - 1) << endl;
+	cout << str1.back() << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	return 0;
+}
+
+// --------------------------------------------------
+
+/// String 추가 사용법 2
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "String";
+	string str2("abcdefghijkl");
+
+	// 확인 출력
+	cout << str1 << endl;
+	cout << str2 << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// --------------------------------------------------
+
+	// begin, end
+	string::iterator iter = str1.begin();
+	for (; iter != str1.end(); iter++)
+	{
+		cout << *iter << ", ";
+	}
+	cout << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// copy
+	char arr[10];
+	int arrLen = str1.copy(arr, 3, 5);
+
+	cout << arrLen << endl;
+	//cout << arr << endl;		// 문자열의 끝이 없어서 Error가 발생한다!
+
+	arr[arrLen] = '\0';			// 문자 끝에 추가해주어야 Error가 발생하지 않는다!
+	cout << arr << endl;
+	cout << "--------------------------------------------------" << endl;
+
+	// compare
+	cout << str1.compare("Stringabcdefghijkl") << endl;		// compare 은 같으면 0, 다르면 -1, 1
+	cout << str1.compare("String") << endl << endl;			// 다르면 -1 (사전순 앞)
+
+	str1 = "bloc";
+	str2 = "blog";
+	cout << str1.compare(str2) << endl;						// 다르면 -1 (사전순 앞)
+	cout << str2.compare(str1) << endl;						// 다르면  1 (사전순 뒤)
+	cout << str1.compare(str1) << endl;						// 같으면  0
+	cout << "--------------------------------------------------" << endl;
+
+	return 0;
+}
+*/
